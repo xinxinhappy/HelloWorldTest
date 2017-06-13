fad59c2aa10adf86c7e2432b8b3754af5561f13a
xin <gxinxin_happy@163.com>
2017-06-13 11:18:51 +0800

    in order to build my packagist

---
+++ src/Tools/HelloWorld/Hello.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2017/6/13
+ * Time: 9:43
+ */
+
+namespace Tools\HelloWorld;
+
+class Hello
+{
+    public static function info()
+    {
+        echo "Hello World<br />";
+    }
+}
